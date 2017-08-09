34c9421d5f3d08ad3edace6851b3ff50e9710ee2
Thomas Cailhol <cailhol.thomas@gmail.com>
2017-08-09 15:12:50 +0200

    restaurants + reviews done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,68 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:           "Epicure au Bristol",
+    address:        "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:   "06 06 06 06 06",
+    category:       "italian"
+  },
+  {
+    name:           "La truffière",
+    address:        "4 rue Blainville 75005 Paris",
+    phone_number:   "06 06 06 06 06",
+    category:       "french"
+  },
+  {
+    name:           "Le pré catelan",
+    address:        "route de Suresnes 75016 Paris",
+    phone_number:   "06 06 06 06 06",
+    category:       "japanese"
+  },
+    {
+    name:           "Pizza Nico",
+    address:        "cours balguerie",
+    phone_number:   "06 06 06 06 06",
+    category:       "italian"
+  },
+    {
+    name:           "Bocca a Bocca",
+    address:        "rue notre dame",
+    phone_number:   "06 06 06 06 06",
+    category:       "italian"
+  }
+]
+
+puts 'Creating reviews...'
+review_attributes = [
+  {
+    content: "This is an amazing restaurant",
+    rating: 5,
+    restaurant_id: 1
+  },
+  {
+    content: "This is an shitty restaurant",
+    rating: 1,
+    restaurant_id: 2
+  },
+  {
+    content: "This is an ok restaurant",
+    rating: 3,
+    restaurant_id: 3
+  },
+  {
+    content: "Pas ouf",
+    rating: 2,
+    restaurant_id: 4
+  },
+  {
+    content: "Super !",
+    rating: 5,
+    restaurant_id: 5
+  }
+]
+Restaurant.create!(restaurants_attributes)
+Review.create!(review_attributes)
+puts 'Finished!'
